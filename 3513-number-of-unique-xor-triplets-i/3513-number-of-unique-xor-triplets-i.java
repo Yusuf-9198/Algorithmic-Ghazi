4fbf51e68b6ecb9e01dd3532926f84e5c1e0a731
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-07-23 22:56:34 +0530

    Time: 0 ms (100%), Space: 125.2 MB (77.55%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,20 +1,23 @@
 // find nearest power of 2 after n only for n>=3
-// class Solution {
-//     public int uniqueXorTriplets(int[] nums) {
-//         int n = nums.length;
-//         if (n<=) return n;
-//         int bits = 32 - Integer.numberOfLeadingZeros(n);
-//         return 1 << bits;
-//     }
-// }
 
+// optimised
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
         if (n<=2) return n;
-        for(int i =1 ; ; i++){
-            if(Math.pow(2,i) > n) return (int)Math.pow(2,i);
-        }
-        
+        int bits = 32 - Integer.numberOfLeadingZeros(n);
+        return 1 << bits;
     }
 }
+
+// // also correct
+// class Solution {
+//     public int uniqueXorTriplets(int[] nums) {
+//         int n = nums.length;
+//         if (n<=2) return n;
+//         for(int i =1 ; ; i++){
+//             if(Math.pow(2,i) > n) return (int)Math.pow(2,i);
+//         }
+        
+//     }
+// }
