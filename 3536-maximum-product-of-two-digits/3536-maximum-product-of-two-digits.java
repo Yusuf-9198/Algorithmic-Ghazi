0a580964d865ec3537e23c63a612b3ad840a6f00
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-07-25 15:39:31 +0530

    Time: 1 ms (100%), Space: 42.5 MB (74.48%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,24 +1,46 @@
-// correct but not optimised
+// // correct but not optimised
+// class Solution {
+//     public int maxProduct(int n) {
+//         if( n < 10) return n;
+//         List<Integer> digits = new ArrayList<>();
+//         while(n>=1){
+//             digits.add(n%10);
+//             n /= 10;
+//         }
+//         int maxPro = 0;
+//         int i = 0 , j=1;
+//         while(i< digits.size()){
+//             if(j< digits.size()){
+//                 maxPro = Math.max(maxPro , digits.get(i) * digits.get(j));
+//                 j++;
+//             }else {
+//                 i++;
+//                 j=i+1;
+//             }
+//         }
+//         return maxPro;
+//     }
+// }
+
+// Also correct
 class Solution {
     public int maxProduct(int n) {
-        if( n < 10) return n;
-        List<Integer> digits = new ArrayList<>();
-        while(n>=1){
-            digits.add(n%10);
+        int max1 = 0; // Largest digit seen so far
+        int max2 = 0; // Second-largest digit seen so far
+        
+        while (n > 0) {
+            int digit = n % 10;
             n /= 10;
-        }
-        int maxPro = 0;
-        int i = 0 , j=1;
-        while(i< digits.size()){
-            if(j< digits.size()){
-                maxPro = Math.max(maxPro , digits.get(i) * digits.get(j));
-                j++;
-            }else {
-                i++;
-                j=i+1;
+            
+            if (digit > max1) {
+                max2 = max1; // Current max becomes second max
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
             }
         }
-        return maxPro;
+        
+        return max1 * max2;
     }
 }
 
