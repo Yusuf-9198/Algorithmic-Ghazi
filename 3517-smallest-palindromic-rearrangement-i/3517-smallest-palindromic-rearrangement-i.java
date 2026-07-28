03acc97bb96ba12f82d79241444e7bd4aa4457c1
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-07-28 22:45:49 +0530

    Time: 32 ms (53.82%), Space: 48.6 MB (10.04%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,33 +1,58 @@
-class Solution {
-    public String sorting(String s) {
-        int n = s.length();
-        char[] charc = s.toCharArray();
-        Arrays.sort(charc);
-        s = "";
-        for (int i = 0; i < n; i++) {
-            s +=charc[i];
-        }
-        return s;
-    }
+// // correct self
+// class Solution {
+//     public String sorting(String s) {
+//         int n = s.length();
+//         char[] charc = s.toCharArray();
+//         Arrays.sort(charc);
+//         s = "";
+//         for (int i = 0; i < n; i++) {
+//             s +=charc[i];
+//         }
+//         return s;
+//     }
 
   
+//     public String smallestPalindrome(String s) {
+//         int n = s.length();
+//         if (n == 1)
+//             return s;
+//         int i = 0, j = n - 1;
+//         String t = "";
+//         String tr = "";
+//         if (n % 2 == 0) {
+//             t = sorting(s.substring(0, (n / 2) ));
+//             tr = new StringBuilder(t).reverse().toString();
+//             return t + tr;
+//         } else {
+//             t = sorting(s.substring(0, n / 2));
+//             tr = new StringBuilder(t).reverse().toString();
+//             return t + s.charAt((n / 2)) + tr;
+
+//         }
+
+//     }
+// }
+import java.util.Arrays;
+
+class Solution {
+    public String sorting(String s) {
+        char[] charc = s.toCharArray();
+        Arrays.sort(charc);
+        return new String(charc); // Fast O(K) conversion
+    }
+
     public String smallestPalindrome(String s) {
         int n = s.length();
-        if (n == 1)
-            return s;
-        int i = 0, j = n - 1;
-        String t = "";
-        String tr = "";
+        if (n == 1) return s;
+
+        // s.substring(0, n / 2) gets the exact left half for both even and odd lengths
+        String t = sorting(s.substring(0, n / 2));
+        String tr = new StringBuilder(t).reverse().toString();
+
         if (n % 2 == 0) {
-            t = sorting(s.substring(0, (n / 2) ));
-            tr = new StringBuilder(t).reverse().toString();
             return t + tr;
         } else {
-            t = sorting(s.substring(0, n / 2));
-            tr = new StringBuilder(t).reverse().toString();
-            return t + s.charAt((n / 2)) + tr;
-
+            return t + s.charAt(n / 2) + tr;
         }
-
     }
 }
