5ebf36f3420e4a4a3692e4ca32a7da7aed01cf5a
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-07-30 15:46:48 +0530

    Add comments for mathematical logic in minimumPushes
    Added comments for mathematical explanation of minimum pushes.

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -52,3 +52,18 @@ class Solution {
         return pushes;
     }
 }
+
+// Also correct mathamatically
+// class Solution {
+//     public int minimumPushes(String word) {
+//         int n = word.length();
+//         int ans = 0;
+        
+//         if (n > 0) ans += Math.min(n, 8) * 1;
+//         if (n > 8) ans += Math.min(n - 8, 8) * 2;
+//         if (n > 16) ans += Math.min(n - 16, 8) * 3;
+//         if (n > 24) ans += (n - 24) * 4;
+        
+//         return ans;
+//     }
+// }
