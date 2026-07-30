8cc42acb6c02e916f084fb407f1cf1062e7b3d23
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-07-30 15:41:20 +0530

    Time: 3 ms (11.24%), Space: 43.6 MB (8.85%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,22 +1,39 @@
+// import java.util.Arrays;
+
+// class Solution {
+//     public int minimumPushes(String word) {
+//         int n = word.length();
+//         int[] fre = new int[26];
+//         int push = 0;
+//         for (int i = 0; i < n; i++) {
+//             fre[word.charAt(i) - 'a']++;
+//         }
+//         Arrays.sort(fre);
+//         int mul = 1;
+//         int k = 25;
+//         while (k >= 0 && fre[k] > 0) {
+//             for (int j = 0; j < 8 && k >= 0 && fre[k] > 0; j++) {
+//                 push += fre[k] * mul;
+//                 k--;
+//             }
+//             mul++;
+//         }
+//         return push;
+//     }
+// }
+
 import java.util.Arrays;
 
 class Solution {
     public int minimumPushes(String word) {
-        int n = word.length();
         int[] fre = new int[26];
-        int push = 0;
-        for (int i = 0; i < n; i++) {
-            fre[word.charAt(i) - 'a']++;
+        for (char c : word.toCharArray()) {
+            fre[c - 'a']++;
         }
         Arrays.sort(fre);
-        int mul = 1;
-        int k = 25;
-        while (k >= 0 && fre[k] > 0) {
-            for (int j = 0; j < 8 && k >= 0 && fre[k] > 0; j++) {
-                push += fre[k] * mul;
-                k--;
-            }
-            mul++;
+        int push = 0;
+        for (int i = 25; i >= 0 && fre[i] > 0; i--) {
+            push += fre[i] * ((25 - i) / 8 + 1);
         }
         return push;
     }
