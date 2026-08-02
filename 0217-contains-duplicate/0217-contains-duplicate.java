2a457ff9a861eef89d7ccdfc247788d54fdd64cf
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-08-03 01:20:40 +0530

    Time: 24 ms (27.53%), Space: 92.9 MB (75.94%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0 ; i< n; i++){
+            set.add(nums[i]);
+        }
+        if(n == set.size()) return false;
+        return true;
+    }
+}
