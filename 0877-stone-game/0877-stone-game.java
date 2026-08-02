cdb2b5daf02f98f79b076478ce5c36475e6cc2a7
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-08-02 14:48:35 +0530

    Time: 7 ms (21.79%), Space: 46.7 MB (16.42%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean stoneGame(int[] nums) {
+        int n = nums.length;
+        int[][] memo = new int[n][n];
+        return maxScoreDiff(nums, 0, n - 1, memo) >= 0;
+    }
+
+    private int maxScoreDiff(int[] nums, int i, int j, int[][] memo) {
+        if (i == j) return nums[i];
+        if (memo[i][j] != 0) return memo[i][j];
+        
+        int takeLeft = nums[i] - maxScoreDiff(nums, i + 1, j, memo);
+        int takeRight = nums[j] - maxScoreDiff(nums, i, j - 1, memo);
+        
+        return memo[i][j] = Math.max(takeLeft, takeRight);
+    
+        
+    }
+}
