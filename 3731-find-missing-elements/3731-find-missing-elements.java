a3b049a5aed54dde652dfb744467e0019795e368
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-08-04 22:21:00 +0530

    Time: 3 ms (81.76%), Space: 46.7 MB (67.71%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,21 +1,38 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        int n = nums.length;
+         int n = nums.length;
         int max = nums[0];
         int min = nums[0];
         List<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
         for(int i =0 ; i< n ; i++){
-            list.add(nums[i]);
+            set.add(nums[i]);
             if(nums[i]> max) max = nums[i];
             if(nums[i] < min) min = nums[i];
         }
-        
-        for(int i =min ; i<=max ; i++){
-            if(list.contains(i)) list.remove(Integer.valueOf(i));
-            else list.add(i);
+        for(int i = min ; i< max ; i++){
+            if(!set.contains(i)) list.add(i);
         }
-
         return list;
-        
     }
 }
+
+// correct( self)
+// class Solution {
+//     public List<Integer> findMissingElements(int[] nums) {
+//         int n = nums.length;
+//         int max = nums[0];
+//         int min = nums[0];
+//         List<Integer> list = new ArrayList<>();
+//         for(int i =0 ; i< n ; i++){
+//             list.add(nums[i]);
+//             if(nums[i]> max) max = nums[i];
+//             if(nums[i] < min) min = nums[i];
+//         }    
+//         for(int i =min ; i<=max ; i++){
+//             if(list.contains(i)) list.remove(Integer.valueOf(i));
+//             else list.add(i);
+//         }
+//         return list; 
+//     }
+// }
