4ab264c1b828288bafd05120d18e752cfe8acd43
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-08-06 19:07:39 +0530

    Time: 1 ms (100%), Space: 42.8 MB (30.89%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -1,18 +1,40 @@
 class Solution {
-    public int pro(int n){
+    private int pro(int n) {
         int product = 1;
-        while(n>0){
-            product *= n%10;
-            n = n/10;
+        while (n > 0) {
+            product *= n % 10;
+            n /= 10;
         }
         return product;
     }
     public int smallestNumber(int n, int t) {
-        int product = pro(n);
-        if(product % t == 0) return n;
-        else{
-            return smallestNumber(++n,t);
+        while (pro(n) % t != 0) {
+            n++;
         }
-        
+        return n;
     }
 }
+
+
+
+
+
+// // correct self
+// class Solution {
+//     public int pro(int n) {
+//         int product = 1;
+//         while (n > 0) {
+//             product *= n % 10;
+//             n = n / 10;
+//         }
+//         return product;
+//     }
+//     public int smallestNumber(int n, int t) {
+//         int product = pro(n);
+//         if (product % t == 0)
+//             return n;
+//         else {
+//             return smallestNumber(++n, t);
+//         }
+//     }
+// }
