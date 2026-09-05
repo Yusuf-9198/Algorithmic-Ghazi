abf29c5e146c9f2c449732168d2cd6cdc7927187
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-09-05 10:35:00 +0530

    Time: 4 ms (88.55%), Space: 133.4 MB (8.01%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -8,20 +8,46 @@
 //  *    keep track of the minimum valid index.
 //  * 5. Return the first (smallest) stable index found, or -1 if no index satisfies the condition.
 //  * TC = O(n)
-class Solution {
-    public int firstStableIndex(int[] nums, int k) {
+class Solution 
+{
+    public int firstStableIndex(int[] nums, int k) 
+    {
         int n = nums.length;
-        int[] prefMax = new int[n];
-        int[] suffMin = new int[n];
-        prefMax[0] = nums[0];
-        for (int i = 1; i < n; i++) prefMax[i] = Math.max(prefMax[i - 1], nums[i]);
-        suffMin[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) suffMin[i] = Math.min(suffMin[i + 1], nums[i]);
-        for (int i = 0; i < n; i++) {
-            if (prefMax[i] - suffMin[i] <= k) return i;
+        int[] mini = new int[n];
+
+        int mint = Integer.MAX_VALUE;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if (nums[i] < mint) mint = nums[i];
+            mini[i] = mint;
         }
+
+        int maxt = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] > maxt) maxt = nums[i];
+            if(maxt - mini[i] <= k) return i;
+        }
+
         return -1;
     }
 }
+
+// // correct and perfect 
+// class Solution {
+//     public int firstStableIndex(int[] nums, int k) {
+//         int n = nums.length;
+//         int[] prefMax = new int[n];
+//         int[] suffMin = new int[n];
+//         prefMax[0] = nums[0];
+//         for (int i = 1; i < n; i++) prefMax[i] = Math.max(prefMax[i - 1], nums[i]);
+//         suffMin[n - 1] = nums[n - 1];
+//         for (int i = n - 2; i >= 0; i--) suffMin[i] = Math.min(suffMin[i + 1], nums[i]);
+//         for (int i = 0; i < n; i++) {
+//             if (prefMax[i] - suffMin[i] <= k) return i;
+//         }
+//         return -1;
+//     }
+// }
 
 
